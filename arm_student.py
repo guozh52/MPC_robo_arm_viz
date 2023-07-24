34adf8c2191f0bb352d9010e99ff3afea320a27d
guozh52 <guozh25@gmail.com>
2023-07-24 19:17:56 -0400

    visualization v1

--- arm_student.py
+++ arm_student.py
@@ -1,10 +1,83 @@
 from arm_dynamics_base import ArmDynamicsBase
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+class Model(nn.Module):
+	def __init__(self, num_links, time_step):
+		super().__init__()
+		self.num_links = num_links
+		self.time_step = time_step
+
+		self.fc1 = nn.Linear(3 * self.num_links, 64)
+		self.fc2 = nn.Linear(64, 64)
+		self.fc3 = nn.Linear(64, 32)
+		self.fc4 = nn.Linear(32, self.num_links)
+
+	def forward(self, x):
+			x = F.relu(self.fc1(x))
+			x = F.relu(self.fc2(x))
+			x = F.relu(self.fc3(x))
+			x = self.fc4(x)
+			return x	
+
+	def compute_next_state(self, state, qddot):
+
+		# Your code goes here
+		new_q = np.zeros(self.num_links)
+		new_qd = np.zeros(self.num_links)
+		next_state = np.zeros(2 * self.num_links)
+		for i in range(self.num_links):
+				new_qd[i] = state[i + self.num_links] + qddot[i] * self.time_step
+				new_q[i] = state[i] + state[i + self.num_links] * self.time_step + 0.5 * qddot[i] * self.time_step * self.time_step
+		for i in range(self.num_links):
+				next_state[i] = new_q[i]
+				next_state[i + self.num_links] = new_qd[i + self.num_links]
+
+		print("next_state:", next_state)
+		return next_state
+
+	def compute_qddot(self, x):
+		pass
+
+class Model2Link(Model):
+		def __init__(self, time_step):
+				super().__init__(2, time_step)
+				# Your code goes here
+				# self.fc1 = nn.Linear(3 * self.num_links, 64)
+				# self.fc2 = nn.Linear(64, 32)
+				# self.fc3 = nn.Linear(32, self.num_links)	
+
+		def compute_qddot(self, x):
+				# Your code goes here
+				self.eval()	
+				features = torch.from_numpy(x).float()
+				qddot = self.forward(features).detach().numpy()
+				return qddot
+		
 class ArmDynamicsStudent(ArmDynamicsBase):
     def init_model(self, model_path, num_links, time_step, device):
-        # dummy impl
-        pass
+
+        self.model = Model2Link(0.01)
+        self.model.load_state_dict(torch.load(model_path))
+        # ---
+        self.model_loaded = True
+
 
     def dynamics_step(self, state, action, dt):
-        # dummy impl
-        return state
+        if self.model_loaded:
+
+            self.model.eval() 
+            input = np.append(state, action)
+            new_acceleration = self.model.compute_qddot(input)
+            new_q = np.zeros(2)
+            new_qd = np.zeros(2)
+            for i in range(2):
+              new_qd[i] = state[i+2] + new_acceleration[i] * 0.01
+              new_q[i] = state[i] + state[i+2] * 0.01 + 0.5 * new_acceleration[i] * 0.01 * 0.01
+            new_state = np.reshape(np.append(new_q, new_qd), (4,1))
+            return new_state
+            # ---
+        else:
+            return state
--- mpc.py
+++ mpc.py
@@ -3,11 +3,71 @@
 # End:
 
 import numpy as np
+from robot import Robot
 
 class MPC:
+
   def __init__(self):
     self.control_horizon = 10
+    self.delta_u = 0.5
+    self.calculate_times = 10   
+    self.du_para = 2  
+
+  def Roll_Out(self, dynamics, state, goal, action):
+    arm = Robot(dynamics)
+    arm.set_state(state)
+    arm.set_action(action)
+    
+    for step_ in range(30):
+      state = arm.get_state()
+      arm.advance()
+      
+    new_state = arm.get_state()
+
+    pos_ee = dynamics.compute_fk(new_state)
+    dist = np.linalg.norm(goal-pos_ee)
+    vel_ee = np.linalg.norm(dynamics.compute_vel_ee(new_state))
+
+    return dist, vel_ee
+
+  def Cost_Func(self, dist, vel_ee):
+    cost = 100 * pow(dist,2) + 4 * pow(vel_ee,2)
+    return cost
+
 
   def compute_action(self, dynamics, state, goal, action):
-    # dummy impl
-    return np.random.uniform(-5.0, 5.0, (dynamics.get_num_links(), 1))
+    # Put your code here. You must return an array of shape (num_links, 1)
+
+    #Roll out and calculate the old cost
+    dist, vel_ee = self.Roll_Out(dynamics, state, goal, action)
+    cost_old = self.Cost_Func(dist, vel_ee)
+    #print("cost", cost_old)
+    
+    #Generate new action
+    for i in range(self.calculate_times):
+      new_action_list = np.zeros((len(action), 2 * len(action)))
+      for num_of_new_act in range(2 * len(action)):
+        new_action_list[:, num_of_new_act] = action[:, 0]
+        if cost_old < self.du_para:
+          if num_of_new_act <= len(action) - 1:
+            new_action_list[num_of_new_act, num_of_new_act] += self.delta_u*(1 - (i/self.calculate_times))*(cost_old/self.du_para)
+          else:
+            new_action_list[num_of_new_act - len(action), num_of_new_act] -= self.delta_u*(1 - (i/self.calculate_times))*(cost_old/self.du_para)
+        else:
+          if num_of_new_act <= len(action) - 1:
+            new_action_list[num_of_new_act, num_of_new_act] += self.delta_u*(1 - (i/self.calculate_times))
+          else:
+            new_action_list[num_of_new_act - len(action), num_of_new_act] -= self.delta_u*(1 - (i/self.calculate_times))
+
+      #Roll out and calculate the new cost function
+      cost_new = np.zeros(new_action_list.shape[1])
+      for i in range(new_action_list.shape[1]):
+        dist, vel_ee = self.Roll_Out(dynamics, state, goal, new_action_list[:, i])
+        cost_new[i] = self.Cost_Func(dist, vel_ee)
+      best_action = np.zeros((new_action_list.shape[0], 1))
+      if np.amin(cost_new) < cost_old:
+        best_action = new_action_list[:, np.argmin(cost_new)].reshape(len(action),1)
+        action = best_action
+        cost_old = np.amin(cost_new)
+  
+    return best_action
